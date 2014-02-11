a6aa7f14030c5fa04e8c426be166c03d2cda7386
mose <mose@mose.com>
2014-02-11 10:41:20 +0800

    made remote install work for rbenv

--- lib/logring/runner.rb
+++ lib/logring/runner.rb
@@ -56,13 +56,16 @@ module Logring
 
     def init(host)
       if @hosts[host]
+        SSHKit.config.command_map[:bundle] = "/usr/local/rbenv/shims/bundle"
         SSHKit::Coordinator.new(@hosts[host]).each do |h|
           if test "[ -d #{h.properties.path} ]"
             info "#{h.properties.name} is already initialized."
           else
             info capture "curl -s -L #{Logring::Config.install_url} | bash -s -- --slave --dest=#{h.properties.path}"
             within h.properties.path do
-              execute :bundle, 'install'
+              with path: '/usr/local/rbenv/shims:$PATH' do
+                execute :bundle, 'install'
+              end
             end
           end
 
